2729ff7f3f88af0ee195b52830a558c4217303bc
Ola Jakobsson <74777302+surfguden@users.noreply.github.com>
2024-10-09 22:21:24 +0200

    Added some plots in matlab script

--- tdms_reade.m
+++ tdms_reade.m
@@ -1,3 +1,5 @@
+close all
+clear all
 fileName = "C:\Users\olaja\Downloads\1e8to1e9MHz_step1kHz_noTcontrol.tdms";
 info = tdmsinfo(fileName);
 synced_channels=info.ChannelList.ChannelName(info.ChannelList.ChannelGroupName=="Untitled")
@@ -5,8 +7,7 @@ all_data=tdmsread(fileName, ChannelGroupName='Untitled', ChannelName=synced_chan
 %% We dont it as cell:
 all_data=all_data{1}
 
-%% calculate elapsed time
-all_data.ElapsedTime=all_data.meTECTime(:)-all_data.meTECTime(1)
+
 %%
 %remove data with function generator off:
 all_data=all_data(all_data.WFGEnabled==1,:)
@@ -17,6 +18,11 @@ for i=1:numel(all_data.Properties.VariableNames)
     str(~ismember(str,['A':'Z' 'a':'z' '0':'9'])) = ''
     all_data.Properties.VariableNames{i}=str
 end
+
+%% calculate elapsed time and correct Impedance (error in labview logger)
+all_data.ElapsedTime=all_data.meTECTime(:)-all_data.meTECTime(1)
+all_data.Impedance=all_data.Ch1Amp./(all_data.Ch2Amp./5);
+all_data.Phase=all_data.Ch1Phase-(all_data.Ch2Phase);
 
 %%
 unique_set_powers=unique(all_data.SetPower)
@@ -47,7 +53,7 @@ end
     plot(unique_set_powers,mean_temp_ch2,"or")
     legend("ch1", "ch2", 'Location',"best")
     xlabel("set power (W)")
-    ylabel("mean temperature (T)")
+    ylabel("mean temperature (C)")
     axis padded
     title("Average Temperature Vs Power")
 
@@ -56,11 +62,22 @@ end
     plot(all_data.MeasuredPower,all_data.meTECCH1ObjectTemperatureC,'DisplayName',"T Ch1 (piezo)","LineStyle", "none", "marker", "x", "color", "red" )
     hold on
     plot(all_data.MeasuredPower,all_data.meTECCH2ObjectTemperatureC,'DisplayName',"T Ch2 (cu film)","LineStyle", "none", "marker", "x", "color", "green" )
-    xlabel("measured power")
-    ylabel("measured Temperature")
+    xlabel("measured power (W)")
+    ylabel("measured Temperature (C)")
     legend('Location',"best")
     axis padded
     title("Temperature vs Measured Power")
+    
+    try %only works in matlab 2024
+    nexttile
+    violinplot(all_data.SetPower,all_data.meTECCH1ObjectTemperatureC)
+    
+    xlabel("set power (W)")
+    ylabel("measured Temperature (C)")
+    legend('Location',"best")
+    axis padded
+    title("Temperature vs Set Power Violin")
+    end
 
     nexttile
     for i=unique_set_powers'
@@ -74,11 +91,62 @@ end
     title("Temperature vs Measured Current")
 
     nexttile
+    for i=unique_set_powers'
+        plot(all_data.Ch1Amp(all_data.SetPower==i),all_data.meTECCH1ObjectTemperatureC(all_data.SetPower==i), 'DisplayName', i +"W","marker", "o","LineStyle", "none")
+        hold on
+    end
+    legend('Location',"best")
+    xlabel("measured Voltage (V)")
+    ylabel("measured Temperature (C)")
+    axis padded
+    title("Temperature vs Measured Voltage")
+
+    nexttile
+    for i=unique_set_powers'
+        plot(all_data.Impedance(all_data.SetPower==i),all_data.meTECCH1ObjectTemperatureC(all_data.SetPower==i), 'DisplayName', i +"W","marker", "o","LineStyle", "none")
+        hold on
+    end
+    legend('Location',"best")
+    xlabel("measured Impedance (U/I)")
+    ylabel("measured Temperature(C)")
+    axis padded
+    title("Temperature vs Measured Impedance (U/I)")
+
+    nexttile
+    for i=unique_set_powers'
+        plot(all_data.Ch1Freq(all_data.SetPower==i),all_data.Impedance(all_data.SetPower==i), 'DisplayName', i +"W","marker", "o","LineStyle", "none")
+        hold on
+    end
+    yyaxis right
+    colororder('default') %%argh
+    hold on
+    for i=unique_set_powers'
+        plot(all_data.Ch1Freq(all_data.SetPower==i),all_data.Phase(all_data.SetPower==i), 'DisplayName', i +"W","marker", ".","LineStyle", "none")
+    end
+
+    legend('Location',"best")
+    xlabel("Frequency (Hz)")
+    yyaxis left
+    ylabel("measured Impedance (U/I)")
+    yyaxis right
+    ylabel("measured phase (deg)")
+    axis padded
+    title("Impedance vs Frequency")
+
+    nexttile
     hold on
     plot(all_data.ElapsedTime, all_data.meTECCH1ObjectTemperatureC)
     plot(all_data.ElapsedTime, all_data.meTECCH2ObjectTemperatureC)
+    ylabel("Temperature(C)")
+    xlabel("Elapsed Time(s)")
     yyaxis right
     plot(all_data.ElapsedTime, all_data.CetoniFillLevel)
+    ylabel("Syringe Fill Level(uL)")
     title("Temperature Vs Elapsed Time, and syringe level")
     legend(["Temperature CH1" "Temperature CH2" "Syringe Fill Level"],'Location',"best")
+    
+
+   
+
+
 
